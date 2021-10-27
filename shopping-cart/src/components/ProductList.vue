109fa09dc02b9bf9256379c30f56d87639d2bf7e
Esteve Tejada <estevetejada@ip-172-16-66-146.eu-west-1.compute.internal>
2021-10-27 17:46:34 +0200

    vuex

--- shopping-cart/src/components/ProductList.vue
+++ shopping-cart/src/components/ProductList.vue
@@ -1,25 +1,43 @@
 <template>
     <div>
-        <h1>Producrt List</h1>
-        <ul>
-            <li v-for="product in products">{{ product.title }} - {{ product.price}} </li>
+        <h1>Product List</h1>
+        <img v-if="loading" src="https://i.imgur.com/SmBqVXA.gif">
+        <ul v-else>
+            <li v-for="product in products" :key="product.id">
+                {{ product.title }} - {{ product.price | currency}} - {{product.inventory}}
+                <button
+                    :disabled="!productIsInStock(product)"
+                    @click="addProductToCart(product)"
+                >Add to cart</button>    
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
-import shop from '@/api/shop';
-
 export default {
     data() {
         return {
-            products: [],
-        };
+            loading: false
+        }
+    },
+    computed: {
+        products() {
+            return this.$store.state.products
+        },
+        productIsInStock() {
+            return this.$store.getters.productIsInStock
+        }
+    },
+    methods: {
+        addProductToCart(product) {
+            this.$store.dispatch('addProductToCart', product)
+        }
     },
     created() {
-        shop.getProducts(products => [
-            this.products = products
-        ])
+        this.loading = true
+        this.$store.dispatch('fetchProducts').
+            then(() => this.loading = false)
     }
 }
 </script>
